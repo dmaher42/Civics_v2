58284fe8f58e0e09fae2b71d371e4b91fa4a4577
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-09-22 21:42:26 +0930

    Revert "Add Athens landmarks overlay and data"

--- src/map/createOverlay.js
+++
@@ -1,7 +0,0 @@
-import { LandmarkOverlay } from './landmarks.js';
-
-function createLandmarkOverlay(canvas, options = {}) {
-  return new LandmarkOverlay(canvas, options);
-}
-
-export { LandmarkOverlay, createLandmarkOverlay };
